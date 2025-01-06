f610e7a730e0201e1e91c113a6b1e76222c44244
YEXIAONAN <yexiaonanair@outlook.com>
2025-01-06 21:05:43 +0800

    Add Code File

---
+++ src/main/java/org/review/ConstructorDemo1.java
@@ -0,0 +1,16 @@
+package org.review;
+
+public class ConstructorDemo1 {
+    String name = "木木老师";
+    int Age = 19;
+
+    public ConstructorDemo1 (String name,int Age) {
+        this.name = name;
+        this.Age = Age;
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
