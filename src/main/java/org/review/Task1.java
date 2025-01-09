0995d4da110df52d5dae2311c62cd4497c1112b9
YEXIAONAN <yexiaonanair@outlook.com>
2025-01-09 21:51:22 +0800

    Add Code File

---
+++ src/main/java/org/review/Task1.java
@@ -0,0 +1,14 @@
+package org.review;
+
+import java.util.Scanner;
+
+public class Task1 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Please Enter Number");
+        int num = sc.nextInt();
+
+        System.out.println(num);
+    }
+}
