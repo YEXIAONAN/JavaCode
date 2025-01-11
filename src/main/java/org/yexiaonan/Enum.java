314c66012a6748df7bb11a076453715c4fcf95f8
YEXIAONAN <yexiaonanair@outlook.com>
2025-01-11 15:55:21 +0800

    Fixing Some Bug

---
+++ src/main/java/org/yexiaonan/Enum.java
@@ -0,0 +1,12 @@
+package org.yexiaonan;
+
+public class Enum {
+    public static void main(String[] args) {
+        // 执行输出结果
+        Color c1 = Color.RED;
+        System.out.println(c1);
+    }
+    enum Color {
+        RED,GREEN,BLUE
+    }
+}
