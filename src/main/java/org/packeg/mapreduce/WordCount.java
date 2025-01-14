73991747086b5e05bd81a79ac80cf7704e21673c
YEXIAONAN <yexiaonanair@outlook.com>
2025-01-14 11:16:35 +0800

    Fixing Some Bug

--- src/main/java/org/packeg/mapreduce/WordCount.java
+++ src/main/java/org/packeg/mapreduce/WordCount.java
@@ -1,10 +1,15 @@
 package org.packeg.mapreduce;
 
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.io.IntWritable;
 import org.apache.hadoop.io.LongWritable;
 import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Job;
 import org.apache.hadoop.mapreduce.Mapper;
 import org.apache.hadoop.mapreduce.Reducer;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
 
 import java.io.IOException;
 
@@ -26,7 +31,26 @@ public class WordCount {
     public static class WordCountReducer extends Reducer<Text,IntWritable,Text,IntWritable>{
         @Override
         protected void reduce(Text key, Iterable<IntWritable> values, Reducer<Text, IntWritable, Text, IntWritable>.Context context) throws IOException, InterruptedException {
+            int sum = 0;
 
+            for (IntWritable val : values){
+                sum = sum + val.get();
+
+            }
+            context.write(key,new IntWritable(sum));
         }
+    }
+
+    public static void main(String[] args) throws IOException, InterruptedException, ClassNotFoundException {
+        Configuration conf = new Configuration();
+        Job job = Job.getInstance(conf,"WordCount");
+        job.setJarByClass(WordCount.class);
+        job.setMapperClass(WordCountMapper.class);
+        job.setReducerClass(WordCountReducer.class);
+        job.setOutputKeyClass(Text.class);
+        job.setOutputValueClass(IntWritable.class);
+        FileInputFormat.addInputPath(job,new Path(args[0]));
+        FileOutputFormat.setOutputPath(job,new Path(args[1]));
+        System.exit(job.waitForCompletion(true)?1:0);
     }
 }
