cff84a5af746f36f731aa12141f8e1036d31023b
YEXIAONAN <yexiaonanair@outlook.com>
2025-01-16 08:57:47 +0800

    Update Code

--- src/main/java/org/packeg/study/StudyIf.java
+++ src/main/java/org/packeg/study/StudyIf.java
@@ -8,22 +8,27 @@ import java.util.Scanner;
 public class StudyIf {
     public static void main(String[] args) {
         // 简单的密码判断（融合密码）
+        // 导入支持包
         Scanner sc = new Scanner(System.in);
 
+        // 获取当前时间，将其Format
         SimpleDateFormat formatter= new SimpleDateFormat("yyyy-MM-dd 'at' HH:mm:ss z");
         Date date = new Date(System.currentTimeMillis());
 
-
+        // 提示文字
         System.out.println("-------------欢迎来到小楠登录系统-------------");
         System.out.println("\t");
         System.out.println("当前时间 " + formatter.format(date));
         System.out.println("\t");
         System.out.println("请输入密码： ");
-        String password = sc.next();
-        sc.toString();
 
+        // 将用户输入的数据存储至一个String类型的变量中
+        String password = sc.next();
+
+        // 定义默认密码
         String data = "yexiaonan";
 
+        // 使用 equals 方法进行比较
         if (Objects.equals(password, data)){
             System.out.println("登录成功");
         }else {
