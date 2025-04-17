e81ee0593a9d77994dedd8a7a48e834817952137
YEXIAONAN <yexiaonanair@outlook.com>
2025-04-17 14:52:18 +0800

    格式化 Test111.java，调整代码缩进以提高可读性

--- src/main/java/org/Test111.java
+++ src/main/java/org/Test111.java
@@ -1,5 +1,19 @@
 package org;
 
 public class Test111 {
-    
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+        System.out.println("This is a test program.");
+
+
+    }
+
+    public static void test() {
+        System.out.println("This is a test method.");
+    } 
+
+
+    public static void test2() {
+        System.out.println("This is another test method.");
+    }
 }
