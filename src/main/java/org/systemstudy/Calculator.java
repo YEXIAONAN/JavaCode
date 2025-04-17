39ac03872e0bd942114bbc07a9e5885135483879
YEXIAONAN <yexiaonanair@outlook.com>
2025-04-17 09:14:27 +0800

    Learn Some

--- src/main/java/org/systemstudy/Calculator.java
+++ src/main/java/org/systemstudy/Calculator.java
@@ -1,5 +1,13 @@
 package org.systemstudy;
 
 public class Calculator {
+    public static double average(int[] numbers) {
+        int sum  = 0;
+        for (int number : numbers) {
+            sum += number;
+        }
+        double avg = (double) sum / numbers.length;
+        return avg;
 
+    }
 }
