826fd6e9ed9263e19b3c3fbced36e0a3bcb2ea70
YEXIAONAN <yexiaonanair@outlook.com>
2025-04-18 15:54:34 +0800

    优化 Test2.java 中的 Scanner 使用，调整代码结构以提高可读性

--- src/main/java/org/packeg/study/Test2.java
+++ src/main/java/org/packeg/study/Test2.java
@@ -5,19 +5,19 @@ import java.util.Scanner;
 
 public class Test2 {
     public static void main(String[] args) {
-        // 导包
-        Scanner sc = new Scanner(System.in);
+        try (// 导包
+        Scanner sc = new Scanner(System.in)) {
+            // 定义默认值
+            String Password = "kk";
 
-        // 定义默认值
-        String Password = "kk";
+            // 接收用户输入数据
+            String data = sc.next();
 
-        // 接收用户输入数据
-        String data = sc.next();
-
-        if (Objects.equals(Password,data)){
-            System.out.println("Yes!");
-        }else {
-            System.out.println("No!");
+            if (Objects.equals(Password,data)){
+                System.out.println("Yes!");
+            }else {
+                System.out.println("No!");
+            }
         }
     }
 }
--- src/main/java/org/yexiaonan/Enum.java
+++ src/main/java/org/yexiaonan/Enum.java
@@ -2,11 +2,14 @@ package org.yexiaonan;
 
 public class Enum {
     public static void main(String[] args) {
-        // 执行输出结果
+        // 创建一个 Color 枚举类型的实例，值为 RED
         Color c1 = Color.RED;
+        // 输出枚举实例的值到控制台
         System.out.println(c1);
     }
+
+    // 定义一个名为 Color 的枚举类型，包含三种颜色：RED, GREEN, BLUE
     enum Color {
-        RED,GREEN,BLUE
+        RED, GREEN, BLUE
     }
 }
