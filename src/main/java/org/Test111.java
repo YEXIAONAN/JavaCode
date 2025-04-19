c532dce054b6a19e433d80bda99d6f2d72a14d5f
Kaedrix <191863253@qq.com>
2025-04-19 14:15:14 +0800

    Test

--- src/main/java/org/Test111.java
+++ src/main/java/org/Test111.java
@@ -4,6 +4,7 @@ public class Test111 {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
         System.out.println("This is a test program.");
+        System.out.println("?");
 
 
     }
@@ -16,4 +17,6 @@ public class Test111 {
     public static void test2() {
         System.out.println("This is another test method.");
     }
+
+
 }
