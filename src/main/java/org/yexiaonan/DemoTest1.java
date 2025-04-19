f4a6d97842b48f36a4a369332ff8a07ed3f295f1
Kaedrix <191863253@qq.com>
2025-04-19 14:12:36 +0800

    Create A Test Deom

---
+++ src/main/java/org/yexiaonan/DemoTest1.java
@@ -0,0 +1,13 @@
+package org.yexiaonan;
+
+public class DemoTest1 {
+    public static void main(String[] args) {
+        System.out.println("HelloWorld!");
+        System.out.println("I am a person");
+
+        int Age = 12;
+        String Name = "XiaoNan";
+
+        System.out.println("Print" +  Age  +  Name);
+    }
+}
