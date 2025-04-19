9200a2ed19dce1c9d51591418d0546c5c03c54e0
Kaedrix <191863253@qq.com>
2025-04-19 14:21:40 +0800

    Add method to calculate sum of three integers

--- src/main/java/org/yexiaonan/MethodFor5.java
+++ src/main/java/org/yexiaonan/MethodFor5.java
@@ -1,5 +1,13 @@
 package org.yexiaonan;
 
+/**
+    * 计算三个整数的和。
+    * 
+    * @param sum1 第一个整数
+    * @param sum2 第二个整数
+    * @param sum3 第三个整数
+    * @return 三个整数的和
+    */
 public class MethodFor5 {
     public static void main(String[] args) {
 
