1ab01a5c737c4342d536dd64ace331935d77f1e4
YEXIAONAN <yexiaonanair@outlook.com>
2025-04-22 21:29:14 +0800

    Remove javadoc

--- src/main/java/org/atguigu/mydoc/member-search-index.js
+++
@@ -1,1 +0,0 @@
-memberSearchIndex = [{"p":"org.atguigu","c":"HelloWorld","l":"HelloWorld()","u":"%3Cinit%3E()"},{"p":"org.atguigu","c":"HelloWorld","l":"main(String[])","u":"main(java.lang.String[])"}];updateSearchResults();
