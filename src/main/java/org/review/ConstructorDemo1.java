001b1ce604f4c646019c096e65cc9c60b5ff5cc8
YEXIAONAN <yexiaonanair@outlook.com>
2025-04-29 17:17:56 +0800

    Refactor constructor and add new classes for enhanced functionality
    Renamed the `name` field to `Name` in `ConstructorDemo1` for consistency and added user input handling using `Scanner`. Introduced new classes `Animal` and `Method` to support additional features, including a test class within `Method` for demonstration purposes.

--- src/main/java/org/review/ConstructorDemo1.java
+++ src/main/java/org/review/ConstructorDemo1.java
@@ -1,16 +1,27 @@
 package org.review;
 
+import java.util.Scanner;
+
 public class ConstructorDemo1 {
-    String name = "木木老师";
+    String Name = "木木老师";
     int Age = 19;
 
     public ConstructorDemo1 (String name,int Age) {
-        this.name = name;
+        this.Name = name;
         this.Age = Age;
         System.out.println();
+
     }
 
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
 
+        System.out.println("input user name");
+        String name = sc.next();
+
+        System.out.println("input user age");
+        int age = sc.nextInt();
     }
+
+
 }
