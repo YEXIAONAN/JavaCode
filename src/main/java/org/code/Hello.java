922da3e76374dbedb3b023f0eda6cd1caf80a24c
YEXIAONAN <yexiaonanair@outlook.com>
2025-05-13 09:25:30 +0800

    Add new classes and documentation for Java basics
    Introduce multiple new classes demonstrating Java features like escape characters, boolean logic, and data formatting, while enhancing the documentation with detailed explanations on Java syntax, code conventions, and data type conversions. Include a new image for data type visualization and remove unused files.

--- src/main/java/org/code/Hello.java
+++ src/main/java/org/code/Hello.java
@@ -1,7 +1,22 @@
 package org.code;
 
+
 public class Hello {
 	public static void main(String[] args) {
-		System.out.println("HelloWorld!");
+		System.out.println("我是小猫猫");
 	}
+}
+
+
+class Dog {
+	public static void main(String[] args) {
+		System.out.println("我是小狗狗");
+	}
+}
+
+class Mugun {
+	public static void main(String[] args) {
+		System.out.println("我是木棍");
+	}
+
 }
