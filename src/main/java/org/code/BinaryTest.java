85b63f0d9b42d1e0b5351b2ff9d4bb865e295e91
YEXIAONAN <yexiaonanair@outlook.com>
2025-05-22 09:14:18 +0800

    学习Java的进制。原码，反码，补码

---
+++ src/main/java/org/code/BinaryTest.java
@@ -0,0 +1,21 @@
+package org.code;
+
+public class BinaryTest {
+    public static void main(String[] args) {
+        // 二进制
+        int n1 = 0b1010;
+
+        // 十进制
+        int n2 = 1010;
+
+        // 八进制
+        int n3 = 01010;
+
+        // 十六进制
+        int n4 = 0x10101;
+        System.out.println(n1);
+        System.out.println(n2);
+        System.out.println(n3);
+        System.out.println(n4);
+    }
+}
---
+++ src/main/java/org/code/Input.java
@@ -0,0 +1,21 @@
+package org.code;
+
+import java.util.Scanner;
+
+public class Input {
+    public static void main(String[] args) {
+        // 导入Scanner获取用户从键盘输入的数据
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("请输入您的姓名：");
+        String name = sc.next();
+        System.out.println("请输入您的年龄：");
+        int age = sc.nextInt();
+        System.out.println("请输入您的薪水：");
+        double money = sc.nextInt();
+
+        System.out.println("您的姓名是：" + name);
+        System.out.println("您的年龄是：" + age);
+        System.out.println("您的薪水是：" + money);
+    }
+}
