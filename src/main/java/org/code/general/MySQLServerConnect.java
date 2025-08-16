3d4f41dfdae796feb282b987fd7eaa73b6569675
yexiaonanair <yexiaonanair@outlook.com>
2025-08-16 20:46:47 +0800

    复习 MySQL 链接

--- src/main/java/org/code/general/MySQLServerConnect.java
+++ src/main/java/org/code/general/MySQLServerConnect.java
@@ -1,23 +1,27 @@
 package org.code.general;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
-import java.sql.Statement;
+import javax.management.Query;
+import java.sql.*;
 
 public class MySQLServerConnect {
 
-    static final String MySQL_URL = "jdbc://192.168.101.251:6666/SDashboard?useSSL=false&allowPublicKeyRetrieval=true";
+    static final String MySQL_URL = "jdbc:mysql://192.168.101.251:6666/SDashboard?useSSL=false&allowPublicKeyRetrieval=true";
     static final String MySQL_USER = "root";
     static final String MySQL_PASSWORD = "BigData#123..";
+    static final String QUERY = "show tables;";
     public static void main(String[] args) throws SQLException {
 
         try (Connection conn = DriverManager.getConnection(MySQL_URL,MySQL_USER,MySQL_PASSWORD);
              Statement stat = conn.createStatement();
         )
         {
-            String sql = "show tables;";
-            stat.execute(sql);
+//            String sql = "show tables;";
+//            stat.execute(sql);
+
+            ResultSet rs = stat.executeQuery(QUERY);
+            while (rs.next()){
+
+            }
         }
         catch (SQLException e){
             e.printStackTrace();
