8dfdc64d1cba5a7ea5c5b3c8043da5ff5fc803f7
yexiaonanair <yexiaonanair@outlook.com>
2025-08-24 16:51:34 +0800

    学习BufferedReader

--- src/main/java/org/code/develop/Tools.java
+++ src/main/java/org/code/develop/Tools.java
@@ -5,21 +5,21 @@ import org.code.tools.MainTools;
 
 import java.util.Scanner;
 
-import static org.code.tools.MainTools.XNInfomation.printAuthor;
+//import static org.code.tools.MainTools.XNInfomation.printAuthor;
 
 public class Tools {
     public static void main(String[] args) {
+        // 创建Scanner对象，获取用户输入
         Scanner sc = new Scanner(System.in);
-        MainTools.setDebug(true);
+
+        // 设置NanTools是否启用Debug模式
+        MainTools.setDebug(false);
 
         String Userinput_1 = sc.next();
 
         String Userinput_2 = sc.next();
 
 
-        System.out.println(MainTools.TextContext(Userinput_1,Userinput_2));
-
-
-        printAuthor();
+        System.out.println(MainTools.TextMatching(Userinput_1,Userinput_2));
     }
 }
--- src/main/java/org/code/tools/MainTools.java
+++ src/main/java/org/code/tools/MainTools.java
@@ -11,17 +11,33 @@ public class MainTools {
 
     // Tools 字符拼接操作
     public static String TextContext(String input_1, String input_2){
+        String TextContent = "字符拼接";
         if(debug){
-            System.out.println("【小楠Tools】你调用了 字符拼接 方法.");
+            System.out.println("【小楠Tools】你调用了 " + TextContent + " 方法.");
         }
         return input_1 + input_2;
     }
 
+    // Tools 传递两个参数进行匹配
+    public static String TextMatching(String input_1, String input_2){
+        String TextMatchingName = "参数匹配";
+        if (debug){
+            System.out.println("【小楠Tools】你调用了 " + TextMatchingName + " 方法.");
+        }
+
+        if (input_1.equals(input_2)){
+            return "【小楠Tools】 匹配成功！";
+        }else {
+            return "【小楠Tools】 匹配失败！";
+        }
+    }
+
     // Tools 输出作者信息
     public static class XNInfomation {
-        public static void printAuthor(){
-            System.out.println("【小楠Tools - 作者信息】");
-            System.out.println("            -by yexiaonan");
+        public static String printAuthor(){
+         return """
+                【小楠Tools】 - v1
+                """;
         }
     }
 }
