a26b48a63cfdc8a46fc8bc206c5c22463a960c5d
yexiaonanair <yexiaonanair@outlook.com>
2025-11-01 00:07:29 +0800

    feth ; 尝试做一个方法类

---
+++ src/main/java/org/code/AccessControllers/MainContort.java
@@ -0,0 +1,26 @@
+package org.code.AccessControllers;
+
+public class MainContort {
+
+    private static class Main{
+
+        public static void Print(){
+            System.out.println("Now loading class method.\n" +
+                    "please waiting ......" +
+                    " __  __       _        ____            _             _   \n" +
+                    "|  \\/  | __ _(_)_ __  / ___|___  _ __ | |_ ___  _ __| |_ \n" +
+                    "| |\\/| |/ _` | | '_ \\| |   / _ \\| '_ \\| __/ _ \\| '__| __|\n" +
+                    "| |  | | (_| | | | | | |__| (_) | | | | || (_) | |  | |_ \n" +
+                    "|_|  |_|\\__,_|_|_| |_|\\____\\___/|_| |_|\\__\\___/|_|   \\__|\n" +
+                    "                                                         ");
+            System.out.println("1");
+        }
+
+    }
+
+
+    public static class MainClass {
+        private final String Info = "the new notice :";
+
+    }
+}
---
+++ src/main/java/org/code/AccessControllers/Test1.java
@@ -0,0 +1,7 @@
+package org.code.AccessControllers;
+
+public class Test1 {
+    public static void main(String[] args) {
+
+    }
+}
