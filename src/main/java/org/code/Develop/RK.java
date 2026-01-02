7c4f2d85ae6db18ff418ea76a90e0c74a18adf82
yexiaonanair <yexiaonanair@outlook.com>
2026-01-02 19:59:10 +0800

    复习

---
+++ src/main/java/org/code/Develop/RK.java
@@ -0,0 +1,11 @@
+package org.code.Develop;
+
+public class RK {
+    public static void main(String[] args) {
+
+    }
+
+    public void PrintInfo() {
+        System.out.println("Thanks you using this");
+    }
+}
