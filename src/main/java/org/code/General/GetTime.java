61d6ea5388d05315fc918a9e5033ac35bb63858b
yexiaonanair <yexiaonanair@outlook.com>
2026-01-14 19:13:40 +0800

    复习

---
+++ src/main/java/org/code/General/GetTime.java
@@ -0,0 +1,13 @@
+package org.code.General;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class GetTime {
+    public static void main(String[] args) {
+        SimpleDateFormat sdf = new SimpleDateFormat();// 格式化时间
+        sdf.applyPattern("yyyy-MM-dd HH:mm:ss a");// a为am/pm的标记
+        Date date = new Date();// 获取当前时间
+        System.out.println(sdf.format(date)); // 输出已经格式化的现在时间（24小时制）
+    }
+}
